8b6e356119bb3309250dc218265e0a563ecc7d12
Jeferson de Souza Andrade <38109358+jefersonae@users.noreply.github.com>
2024-06-02 20:44:33 -0300

    create stardand config

--- src/main/java/com/forum/ExemploDeForumApplication.java
+++ src/main/java/com/forum/ExemploDeForumApplication.java
@@ -1,4 +1,4 @@
-package com.forum.forum;
+package com.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/forum/ExemploDeForumApplicationTests.java
+++ src/test/java/com/forum/ExemploDeForumApplicationTests.java
@@ -1,4 +1,4 @@
-package com.forum.forum;
+package com.forum;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
